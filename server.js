62b8b3163a3539b42a99590442ee2546e2a83c98
Rohan <rohan.rd07@gmail.com>
2022-10-20 11:57:30 +0530

    renames, .gitignore, new files and server created

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require('express') //Creating a variable express and adding express module into app.
 const app = express() //Calling express
+const ejs = require('ejs')
+const expressLayout = require('express-ejs-layouts')
+//Importing ejs and express-js-layouts. Both are just js templates that we will be using...
+
+const path = require('path')
 
 const PORT = process.env.PORT || 3300
 // PORT variable will be either assigned with process.env which is currently available environment port number or the port number will be 3000. In short we are setting sn environment variable.
 // PORT mila toh PORT variable mein port rahega
 
-app.get('/', function(req, res)=>{
-    res.send("Hello, your server here...")
+
+app.get('/', (req, res) => {
+    res.render('home')
 })
 
+app.use(expressLayout)
+app.set('views', path.join(__dirname, '/resources/views'))
+//Here we will set the file path of views folder which will contain how will the site look like
+app.set('view engine', 'ejs')
 /*
 if(process.env.PORT)
 {
