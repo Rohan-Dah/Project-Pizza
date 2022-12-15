d5b08c9f1c54dede2f919e8c77748e60e5a5f793
Rohan <rohan.rd07@gmail.com>
2022-12-15 18:20:26 +0530

    New static images uploaded

--- routes/web.js
+++ routes/web.js
@@ -7,27 +7,14 @@ const cartController = require('../App/http/controllers/customers/cartController
 function initRoutes(app){
 
     app.get('/', homeController().index)
-    //Second parameter is a factory function which means it completely redirects the req, res block and returns what the function actually returns.
-    
-    // app.get('/', (req, res) => {
-    //     res.render('home')
-    // })
 
     app.get('/login', authController().login)
     //Here authController is the factory function which is at /App/controllers/home
 
     app.get('/register', authController().register)
     
-    app.get('/cart', cartController().index)
-
-    // app.get('/cart', (req, res) => {
-    //     res.render('customers/cart')
-    // }) 
-    
-    
+    app.get('/cart', cartController().index)  
 }
 
 module.exports = initRoutes
 
-//Here we will be recieving parameter called which is under the domain of require express module that is in the server file. 
-//The point to note is every file in node is a module which can be imported to any other file by simply using require. 
--- server.js
+++ server.js
@@ -1,16 +1,13 @@
-const express = require('express') //Creating a variable express and adding express module into app.
-const app = express() //Calling express
+const express = require('express') 
+const app = express() 
 const ejs = require('ejs')
 const expressLayout = require('express-ejs-layouts')
-//Importing ejs and express-js-layouts. Both are just js templates that we will be using...
+
 
 const path = require('path')
 
 const PORT = process.env.PORT || 3000
-// PORT variable will be either assigned with process.env which is currently available environment port number or the port number will be 3000. In short we are setting sn environment variable.
-// PORT mila toh PORT variable mein port rahega
 
-// This is used to direct the server to the cart page.
 const { colors } = require('laravel-mix/src/Log')
 
 const mongoose = require('mongoose');
@@ -18,9 +15,7 @@ const mongoose = require('mongoose');
 const url = 'mongodb://localhost/pizza';
 mongoose.connect(url, {
     useNewUrlParser: true,
-    // useCreateIndex: true,
     useUnifiedTopology: true,
-    // useFindAndModify: true
 });
 const connection = mongoose.connection;
 connection.once('open', () => {
@@ -37,25 +32,10 @@ app.use(express.static('public'))
 app.use(expressLayout)
 app.set('views', path.join(__dirname, '/resources/views'))
 
-//Here we will set the file path of views folder which will contain how will the site look like
 app.set('view engine', 'ejs')
 
 require('./routes/web')(app)
-//Here app is being sent by reference
-
-/*
-if(process.env.PORT)
-{
-    PORT = process.env.PORT
-} //This is where we are assigning the value of the port number to the environment variable.
-
-else{
-    PORT = 3000
-}
-*/
 
 app.listen(PORT, () => {
     console.log(`Listening on Port ${PORT}`)
 })
-//app.listen is used to listen and bind the connections on host and port. This is similar to Node's http.server.listen
-//After fetching the port number it will display the below text.
